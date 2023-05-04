9ede48d7b115b97ef7ee3b7c766e043c6298ada8
RoMeRO93 <122970841+RoMeRO93@users.noreply.github.com>
2023-05-04 15:30:43 +0300

    Add project files.

---
+++ my event 1.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Example
+{
+    public delegate string demoDelegate(string str1, string str2);
+    class MyEvents
+    {
+        event demoDelegate myEvent;
+        public MyEvents()
+        {
+            this.myEvent += new demoDelegate(this.Display);
+        }
+        public string Display(string studentname, string subject)
+        {
+            return "Mentorul meu: " + studentname + "\nStatutul: " + subject;
+        }
+        static void Main(string[] args)
+        {
+            MyEvents e = new MyEvents();
+            string res = e.myEvent("Paul", "Maestrul");
+            Console.WriteLine("REZULTATUL Studiului...\n" + res);
+        }
+    }
+}
